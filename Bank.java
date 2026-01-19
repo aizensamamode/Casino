6b2c35bec8d5904e2127747c10e018bb1e141d91
adminsprobag <marknel.jayme21@gmail.com>
2026-01-20 05:51:30 +0800

    latest jan 20

--- Bank.java
+++ Bank.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
 public class Bank {
+    Scanner scan;
     String bankname = "Mark";
     int bankmoney   = 100;
     
+    Bank(Scanner scan){
+        this.scan = scan;
+    }
+
     void bankshow1(){
         System.out.println("----------------------------");
         System.out.println("[BANK ACCOUNT]");
         System.out.println("[NAME : "+this.bankname+" ]");
         System.out.println("[MONEY: "+this.bankmoney+" ]");
         System.out.println("----------------------------");
+        scan.nextLine();
         //AFTER HERE THERE SHOULD BE SCANNER WHEN USER PRESS ENTER IT PROCED TO NEXT
     }
 }
--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    Bank bank = new Bank();
+    Bank bank = new Bank(scan);
     
     bank.bankshow1(); 
     /* PROBLEM 1. WE NEED TO JUST CREATE ONE SCANNER AND USE IT IN EVERY CLASS
      FIX THIS PROBLEM */ 
      System.out.println("problem");
+     scan.close();
 
     }
 }
